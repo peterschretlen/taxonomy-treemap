a4784dcf3f32eb89d13e65a67218f0179d8789b5
pschretlen <peter.schretlen@gmail.com>
2015-11-27 23:32:22 -0500

    create treemap html widget

--- scrape_category_counts.R
+++ scrape_category_counts.R
@@ -1,3 +1,8 @@
+library(treemap)
+library(dplyr)
+# devtools::install_github("timelyportfolio/d3treeR")
+library(d3treeR)
+
 source("taxonomy.R")
 source("scrape.R")
 
@@ -5,11 +10,39 @@ taxonomy <- load_taxonomy("data/CopyOftaxonomy_11Nov2015.json")
 
 #TODO: add a root entry to get all products
 
-subset <- taxonomy[1:nrow(taxonomy),]
-
-urls <- category_url(subset$path)
+urls <- category_url(taxonomy$path)
+taxonomy$url <- urls
 
 filename <- "taxonomy.csv"
 file.create(filename)
 
 counts_and_listigs_all <- adply(urls, c(1), function(x) scrape(x, filename), .progress = "text" )
+
+taxonomy_counts <- read.table("taxonomy.csv",sep = '~', stringsAsFactors = FALSE)
+
+#fix problem where column headers are output on every line
+taxonomy_counts <- taxonomy_counts %>% filter( taxonomy_counts$V1 != "url") 
+names(taxonomy_counts) <- c("url","count", "listings", "facet_data")
+
+#fix problem with duplicate rows(???)
+taxonomy_counts <- taxonomy_counts[!duplicated(taxonomy_counts),]
+
+#join back to original data
+taxonomy <- left_join(taxonomy, taxonomy_counts, by = c("url") )
+
+taxonomy$parent_id[is.na(taxonomy$parent_id)] <- 0
+taxonomy$count[is.na(taxonomy$count)] <- 0
+
+taxonomy_levels <- as.data.frame(str_split_fixed(taxonomy$path, "\\.", 7))
+names(taxonomy_levels) <- paste("pathlevel", 1:7, sep="")
+
+taxonomy <- cbind(taxonomy, taxonomy_levels)
+
+taxonomy$count <- as.numeric(taxonomy$count)
+
+
+tm <- treemap(taxonomy, index=paste("pathlevel", 1:7, sep=""), vSize="count")
+widget <- d3tree3( tm, rootname = "Etsy" )
+saveWidget(widget, "etsy-treemap3.html", selfcontained = FALSE)
+
+
